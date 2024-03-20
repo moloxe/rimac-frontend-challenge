a8fc50b265d3aa610569fc78c7889ab141e1476d
Franco Moloche <dev@moloxe.io>
2024-03-20 02:00:12 -0500

    Add mobile version of plan steps progress bar

--- src/routes/plan-steps/plan-steps-progress/index.tsx
+++ src/routes/plan-steps/plan-steps-progress/index.tsx
@@ -1,6 +1,8 @@
 import Step from './step'
 import StepSeparator from '@/assets/images/plan-steps-page/step-separator.svg'
 import { FC } from 'react'
+import BackButtonOpaque from '@/assets/images/icons/back-button-opaque.svg'
+import useStore from '@/hooks/use-store'
 import './index.scss'
 
 type Props = {
@@ -8,12 +10,38 @@ type Props = {
 }
 
 const PlanStepsProgress: FC<Props> = ({ isFinished }) => {
+  const currentStep = isFinished ? 2 : 1
+  const { setChosenPlan, setUserData } = useStore()
+
+  function backToFirstStep() {
+    if (isFinished) setChosenPlan(null)
+    else setUserData(null)
+  }
+
   return (
-    <div className="plan-steps-progress">
-      <Step step="1" title="Planes y coberturas" active={!isFinished} />
-      <img src={StepSeparator} />
-      <Step step="2" title="Resumen" active={isFinished} />
-    </div>
+    <>
+      <div className="plan-steps-progress-mobile">
+        <button
+          onClick={backToFirstStep}
+          className="plan-steps-progress-mobile--back-button"
+        >
+          <img src={BackButtonOpaque} />
+        </button>
+        <p className="plan-steps-progress-mobile--steps">
+          Paso {currentStep} de 2
+        </p>
+        <div
+          className={`plan-steps-progress-mobile--progress-bar${
+            isFinished ? '-finished' : ''
+          }`}
+        />
+      </div>
+      <div className="plan-steps-progress">
+        <Step step="1" title="Planes y coberturas" active={!isFinished} />
+        <img src={StepSeparator} />
+        <Step step="2" title="Resumen" active={isFinished} />
+      </div>
+    </>
   )
 }
 
